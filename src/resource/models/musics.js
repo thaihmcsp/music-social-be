320853528cdfb9627849816b6b60b72031541f4d
duyth <duyth@tokyotecklab.com>
2023-03-12 21:41:19 +0700

    123

--- src/resource/models/musics.js
+++ src/resource/models/musics.js
@@ -2,41 +2,41 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const MusicSchema = new Schema({
-    musicName: {
-        type: String,
-        required: true,
-        unique: true,
+  musicName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  musicAuthor: {
+    type: String,
+  },
+  musicImg: {
+    type: String,
+    contentType: String,
+  },
+  musicFile: {
+    type: String,
+    contentType: String,
+  },
+  musicCategory: {
+    type: Schema.Types.String,
+    ref: "categories",
+  },
+  musicLike: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "users",
     },
-    musicAuthor: {
-        type: String,
-    },
-    musicImg: {
-        type: String,
-        contentType: String,
-    },
-    musicFile: {
-        type: String,
-        contentType: String,
-    },
-    musicCategory: {
-        type: Schema.Types.String,
-        ref: "categories",
-    },
-    musicLike: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "users",
-        },
-    ],
-    userId: {
-        type: Schema.Types.ObjectId,
-        ref: "users",
-        default: "61600aa78b331c8efe5a3d84",
-    },
-    musicCreationDate: {
-        type: Date,
-        default: Date.now,
-    },
+  ],
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "users",
+    default: "61600aa78b331c8efe5a3d84",
+  },
+  musicCreationDate: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model("musics", MusicSchema);
