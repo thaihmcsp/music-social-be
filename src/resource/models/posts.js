c2b8f58336e719cce9648a655d08c116a19da03b
thaihmcsp <thaidd7b@gmail.com>
2023-03-17 22:23:09 +0700

    like post API

--- src/resource/models/posts.js
+++ src/resource/models/posts.js
@@ -16,6 +16,7 @@ const PostSchema = new Schema({
     type: Date,
     default: Date.now,
   },
+  like: [{ type: Schema.Types.ObjectId, ref: "users" }],
 });
 
 module.exports = mongoose.model("posts", PostSchema);
